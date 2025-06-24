df9bf4381b750ccad1b2e01eb4e4fda27723810d
Eduardo Del Valle <eduardodelvalle_90@hotmail.com>
2025-06-24 20:21:42 -0300

    primer app

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import saludo
+from .views import *
 
 urlpatterns = [
     path('hola-mundo/', saludo),
+    path('hola-mundo-template/', saludo_con_template),
 ]
--- mi_primer_app/views.py
+++ mi_primer_app/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def saludo(request):
     return HttpResponse("¡Hola, mundo!")
+
+def saludo_con_template(request):
+    return render(request, 'mi_primer_app/saludo.html')
