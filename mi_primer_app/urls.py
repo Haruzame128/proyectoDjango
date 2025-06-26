9de5208e10011045797ed6e8c4ebb0bbbc6b8afb
Eduardo Del Valle <eduardodelvalle_90@hotmail.com>
2025-06-26 20:11:54 -0300

    ultimos cambios

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import saludo, saludo_con_template, crearFamiliar
 
 urlpatterns = [
     path('hola-mundo/', saludo),
     path('hola-mundo-template/', saludo_con_template),
+    path('crear-familiar/<str:nombre>/', crearFamiliar), # type: ignore
 ]
--- mi_primer_app/views.py
+++ mi_primer_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Familiar
 
 def saludo(request):
     return HttpResponse("¡Hola, mundo!")
 
 def saludo_con_template(request):
     return render(request, 'mi_primer_app/saludo.html')
+
+def crearFamiliar(request, nombre):
+    if nombre is not None:
+        nuevo_familiar = Familiar(nombre=nombre, apellido="Apellido", edad=30, fecha_nacimiento="1993-01-01", parentesco="Primo")
+        nuevo_familiar.save()
+        return render(request, 'mi_primer_app/crear-familiar.html', {'nombre': nombre})
